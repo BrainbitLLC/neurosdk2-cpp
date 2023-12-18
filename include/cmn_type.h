ea127ccb7961b31460d31b6b47884122a179e638
aseatari <aseatari@gmail.com>
2023-12-18 15:45:07 +0300

    Update lib includes

--- include/cmn_type.h
+++ include/cmn_type.h
@@ -20,16 +20,7 @@
 #ifndef SENSOR_CHANNEL_NAME_LEN
 #define SENSOR_CHANNEL_NAME_LEN 8
 #endif // !SENSOR_CHANNEL_NAME_LEN
-#ifndef NEURO_EEG_MAX_CH_COUNT
-#define NEURO_EEG_MAX_CH_COUNT 24
-#endif // !NEURO_EEG_MAX_CH_COUNT
-#ifndef FILE_NAME_MAX_LEN
-#define FILE_NAME_MAX_LEN 64
-#endif // !FILE_NAME_MAX_LEN
 
-#ifndef NEURO_BAM_MAX_CH_COUNT
-#define NEURO_BAM_MAX_CH_COUNT 8
-#endif // !NEURO_BAM_MAX_CH_COUNT
 
 
 typedef struct _OpStatus
@@ -46,18 +37,18 @@ enum SensorFamily : uint8_t
 	SensorLEKolibri = 2,
 	SensorLEBrainBit = 3,
 	SensorLEBrainBitBlack = 4,
-	SensorLEHeadPhones = 5,
-	SensorLEHeadPhones2 = 6,
+
+
 	SensorLESmartLeg = 7,
 	SensorLENeurro = 8,
 	SensorLEP300 = 9,
 	SensorLEImpulse = 10,
-	SensorLEHeadband = 11,
+
 	SensorLEEarBuds = 12,
-	SensorSPCompactNeuro = 13,
-	SensorLENeuroEEG = 14,
-	SensorLECallibriNext = 15,
-	SensorLENeuroBAM = 16,
+
+
+
+
 };
 
 typedef struct _SensorVersion {
@@ -80,6 +71,7 @@ typedef struct _SensorInfo
 	char Address[SENSOR_ADR_LEN];
 	char SerialNumber[SENSOR_SN_LEN];
 	uint8_t PairingRequired;
+	int16_t RSSI;
 } SensorInfo;
 
 enum SensorFeature : int8_t
@@ -298,25 +290,6 @@ typedef struct _BrainBitResistData {
 typedef void* BrainBitSignalDataListenerHandle;
 typedef void* BrainBitResistDataListenerHandle;
 
-typedef struct _HeadbandSignalData {
-	uint32_t PackNum;
-	uint8_t Marker;
-	double O1;
-	double O2;
-	double T3;
-	double T4;
-} HeadbandSignalData;
-
-typedef struct _HeadbandResistData {
-	uint32_t PackNum;
-	double O1;
-	double O2;
-	double T3;
-	double T4;
-} HeadbandResistData;
-
-typedef void* HeadbandSignalDataListenerHandle;
-typedef void* HeadbandResistDataListenerHandle;
 
 enum IrAmplitude : uint8_t {
 	IrAmp0 = 0,
@@ -342,109 +315,8 @@ enum RedAmplitude : uint8_t {
 	RedAmpUnsupported = 0xFF
 };
 
-enum GenCurrent : uint8_t {
-	GenCurr0nA = 0,
-	GenCurr6nA = 1,
-	GenCurr12nA = 2,
-	GenCurr18nA = 3,
-	GenCurr24nA = 4,
-	GenCurr6uA = 5,
-	GenCurr24uA = 6,
-	Unsupported = 0xFF
-};
 
-typedef struct _HeadphonesSignalData {
-	uint32_t PackNum;
-	uint8_t Marker;
-	double Ch1;
-	double Ch2;
-	double Ch3;
-	double Ch4;
-	double Ch5;
-	double Ch6;
-	double Ch7;
-} HeadphonesSignalData;
 
-typedef struct _HeadphonesResistData {
-	uint32_t PackNum;
-	double Ch1;
-	double Ch2;
-	double Ch3;
-	double Ch4;
-	double Ch5;
-	double Ch6;
-	double Ch7;
-} HeadphonesResistData;
-
-typedef struct _HeadphonesAmplifierParam {
-	uint8_t ChSignalUse1;
-	uint8_t ChSignalUse2;
-	uint8_t ChSignalUse3;
-	uint8_t ChSignalUse4;
-	uint8_t ChSignalUse5;
-	uint8_t ChSignalUse6;
-	uint8_t ChSignalUse7;
-
-	uint8_t ChResistUse1;
-	uint8_t ChResistUse2;
-	uint8_t ChResistUse3;
-	uint8_t ChResistUse4;
-	uint8_t ChResistUse5;
-	uint8_t ChResistUse6;
-	uint8_t ChResistUse7;
-
-	SensorGain ChGain1;
-	SensorGain ChGain2;
-	SensorGain ChGain3;
-	SensorGain ChGain4;
-	SensorGain ChGain5;
-	SensorGain ChGain6;
-	SensorGain ChGain7;
-
-	GenCurrent Current;
-} HeadphonesAmplifierParam;
-
-typedef void* HeadphonesSignalDataListenerHandle;
-typedef void* HeadphonesResistDataListenerHandle;
-
-typedef struct _Headphones2SignalData {
-	uint32_t PackNum;
-	uint8_t Marker;
-	double Ch1;
-	double Ch2;
-	double Ch3;
-	double Ch4;
-} Headphones2SignalData;
-
-typedef struct _Headphones2ResistData {
-	uint32_t PackNum;
-	double Ch1;
-	double Ch2;
-	double Ch3;
-	double Ch4;
-} Headphones2ResistData;
-
-typedef struct _Headphones2AmplifierParam {
-	uint8_t ChSignalUse1;
-	uint8_t ChSignalUse2;
-	uint8_t ChSignalUse3;
-	uint8_t ChSignalUse4;
-
-	uint8_t ChResistUse1;
-	uint8_t ChResistUse2;
-	uint8_t ChResistUse3;
-	uint8_t ChResistUse4;
-
-	SensorGain ChGain1;
-	SensorGain ChGain2;
-	SensorGain ChGain3;
-	SensorGain ChGain4;
-
-	GenCurrent Current;
-} Headphones2AmplifierParam;
-
-typedef void* Headphones2SignalDataListenerHandle;
-typedef void* Headphones2ResistDataListenerHandle;
 
 enum SensorAmpMode : uint8_t
 {
@@ -500,253 +372,9 @@ typedef struct _MEMSData {
 
 typedef void* MEMSDataListenerHandle;
 
-enum EEGChannelType : uint8_t
-{
-	EEGChTypeSingleA1,
-	EEGChTypeSingleA2,
-	EEGChTypeDifferential,
-	EEGChTypeRef
-};
-
-enum EEGChannelId : uint8_t
-{
-	EEGChIdUnknown,
-	EEGChIdO1,
-	EEGChIdP3,
-	EEGChIdC3,
-	EEGChIdF3,
-	EEGChIdFp1,
-	EEGChIdT5,
-	EEGChIdT3,
-	EEGChIdF7,
-
-	EEGChIdF8,
-	EEGChIdT4,
-	EEGChIdT6,
-	EEGChIdFp2,
-	EEGChIdF4,
-	EEGChIdC4,
-	EEGChIdP4,
-	EEGChIdO2,
-
-	EEGChIdD1,
-	EEGChIdD2,
-	EEGChIdOZ,
-	EEGChIdPZ,
-	EEGChIdCZ,
-	EEGChIdFZ,
-	EEGChIdFpZ,
-	EEGChIdD3
-};
-
-typedef struct _EEGChannelInfo
-{
-	EEGChannelId Id;
-	EEGChannelType ChType;
-	char Name[SENSOR_CHANNEL_NAME_LEN];
-	uint8_t Num;
-} EEGChannelInfo;
-
-enum EEGChannelMode : uint8_t {
-	EEGChModeOff,
-	EEGChModeShorted,
-	EEGChModeSignalResist,
-	EEGChModeSignal,
-	EEGChModeTest
-};
-
-typedef struct _SignalChannelsData {
-	uint32_t PackNum;
-	uint8_t Marker;
-	uint32_t SzSamples;
-	double* Samples;
-} SignalChannelsData;
-
-
-typedef struct _CompactNeuroStimulParam
-{
-	double Freq;
-	int32_t PulseWidthMs;
-	double FillingFreq;
-	uint8_t Power;
-	uint16_t Count;
-} CompactNeuroStimulParam;
-
-enum CompactNeuroSignalMarker : uint8_t
-{
-	None = 0x00,
-	PhotoStimul = 0x01,
-	AcousticStimul = 0x02,
-	LostFrame = 0x80
-};
-
-typedef struct _CompactNeuroSignalData
-{
-	uint32_t PackNum;
-	CompactNeuroSignalMarker Marker;
-	double O1;
-	double P3;
-	double C3;
-	double F3;
-	double Fp1;
-	double T5;
-	double T3;
-	double F7;
-
-	double F8;
-	double T4;
-	double T6;
-	double Fp2;
-	double F4;
-	double C4;
-	double P4;
-	double O2;
-
-	double D1;
-	double D2;
-	double OZ;
-	double PZ;
-	double CZ;
-	double FZ;
-	double FpZ;
-	double D3;
-} CompactNeuroSignalData;
-
-typedef struct _CompactNeuroResistData
-{
-	double O1;
-	double P3;
-	double C3;
-	double F3;
-	double Fp1;
-	double T5;
-	double T3;
-	double F7;
-
-	double F8;
-	double T4;
-	double T6;
-	double Fp2;
-	double F4;
-	double C4;
-	double P4;
-	double O2;
-
-	double D1;
-	double D2;
-	double OZ;
-	double PZ;
-	double CZ;
-	double FZ;
-	double FpZ;
-	double D3;
-} CompactNeuroResistData;
 
 
 
-typedef void* CompactNeuroSignalDataListenerHandle;
-typedef void* CompactNeuroResistDataListenerHandle;
-
-enum EEGRefMode : uint8_t
-{
-	RefHeadTop = 1,
-	RefA1A2
-};
-
-typedef struct _NeuroEEGAmplifierParam {
-	uint8_t ReferentResistMesureAllow;
-	SensorSamplingFrequency Frequency;
-	EEGRefMode ReferentMode;
-	EEGChannelMode ChannelMode[NEURO_EEG_MAX_CH_COUNT];
-	SensorGain ChannelGain[NEURO_EEG_MAX_CH_COUNT];
-} NeuroEEGAmplifierParam;
-
-enum SensorFSStatus : uint8_t
-{
-	FSStatusOK,
-	FSStatusNoInit,
-	FSStatusNoDisk,
-	FSStatusProtect
-};
-
-enum SensorFSIOStatus : uint8_t
-{
-	FSIOStatusNoError,
-	FSIOStatusIOError,
-	FSIOStatusTimeout
-};
-
-enum SensorFSStreamStatus : uint8_t
-{
-	FSStreamStatusClosed,
-	FSStreamStatusWrite,
-	FSStreamStatusRead
-};
-
-typedef struct _NeuroEEGFSStatus {
-	SensorFSStatus Status;
-	SensorFSIOStatus IOStatus;
-	SensorFSStreamStatus StreamStatus;
-	uint8_t AutosaveSignal;
-} NeuroEEGFSStatus;
-
-typedef struct _SensorFileInfo {
-	char FileName[FILE_NAME_MAX_LEN];
-	uint32_t FileSize;
-	uint16_t ModifiedYear;
-	uint8_t ModifiedMonth;
-	uint8_t ModifiedDayOfMonth;
-	uint8_t ModifiedHour;
-	uint8_t ModifiedMin;
-	uint8_t ModifiedSec;
-	uint8_t Attribute;
-} SensorFileInfo;
-
-typedef struct _SensorFileData {
-	uint32_t OffsetStart;
-	uint32_t DataAmount;
-	uint32_t SzData;
-	uint8_t* Data;
-} SensorFileData;
-
-typedef struct _SensorDiskInfo {
-	uint64_t TotalSize;
-	uint64_t FreeSize;
-} SensorDiskInfo;
-
-typedef struct _ResistChannelsData {
-	uint32_t PackNum;
-	double A1;
-	double A2;
-	double Bias;
-	uint32_t SzValues;
-	double* Values;
-} ResistChannelsData;
-
-typedef void* NeuroEEGSignalDataListenerHandle;
-typedef void* NeuroEEGResistDataListenerHandle;
-typedef void* NeuroEEGSignalResistDataListenerHandle;
-typedef void* NeuroEEGSignalRawDataListenerHandle;
-typedef void* NeuroEEGFileStreamDataListenerHandle;
-typedef void* NeuroEEGSignalProcessParam;
-
-typedef struct _NeuroBAMAmplifierParam {
-	SensorSamplingFrequency Frequency;
-	EEGChannelMode ChannelMode[NEURO_BAM_MAX_CH_COUNT];
-	SensorGain ChannelGain[NEURO_BAM_MAX_CH_COUNT];
-} NeuroBAMAmplifierParam;
-
-typedef struct _NeuroBAMResistChannelsData {
-	uint32_t PackNum;
-	double Fp1;
-	double Fp2;
-	uint32_t SzValues;
-	double* Values;
-} NeuroBAMResistChannelsData;
-
-typedef void* NeuroBAMSignalDataListenerHandle;
-typedef void* NeuroBAMResistDataListenerHandle;
-typedef void* NeuroBAMSignalResistDataListenerHandle;
 
 enum SensorADCInput : int8_t {
 	ADCInputElectrodes,
@@ -864,21 +492,6 @@ typedef void* CallibriElectrodeStateListenerHandle;
 typedef void* CallibriEnvelopeDataListenerHandle;
 typedef void* QuaternionDataListenerHandle;
 
- 
-enum CallibriNextChCfg : uint8_t
-{
-	ChCfgRectification = 0x01,
-	ChCfgTest = 0x02,
-	ChCfgUnknown = 0xFF
-};
-typedef struct _CallibriNextChannelData {
-	uint32_t PackNum;
-	uint32_t SzSamples;
-	double* Samples;
-} CallibriNextChannelData;
-
-typedef void* CallibriNextChannelDataListenerHandle;
-typedef void* CallibriNextElectrodeStateListenerHandle;
 
 
 typedef struct _SensorScanner SensorScanner;
